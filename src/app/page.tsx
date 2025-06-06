1118f3a32a816445d974369499636cea95351d3a
Maxence Agullo <magullo35@gmail.com>
2025-06-06 10:56:22 +0200

    final test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,10 @@ export default function Home() {
         <p className={styles.intro}>Bienvenue au royaume des patins<br/>Depuis les années 80 le monde du transport et du sport n&apos;a cessé d&apos;évoluer. Chez HyperSkate nous avons trouvé comment subvenir à tout vos besoins.<br/>Nous vous proposons une large gamme de patins à roulettes pour tout les sports que vous pouvez pratiquer.<br/>N&apos;hésitez plus, optez pour HyperSkate.</p>
         <section className={styles.presentation}>
           <img src="/images/product.png" className={styles.productPhoto} />
-          <p>Après des années de recherche, de prototypes secrets et de tests intensifs, le projet voit enfin le jour : découvrez les tout premiers patins à roulettes spécialement conçus pour des sports qui n’en avaient pas besoin… jusqu’à aujourd’hui.</p>
+          <p className={styles.intro2}>Après des années de recherche, de prototypes secrets et de tests intensifs, le projet voit enfin le jour : découvrez les tout premiers patins à roulettes spécialement conçus pour des sports qui n’en avaient pas besoin… jusqu’à aujourd’hui.</p>
         </section>
         <Products />
+        <img src="https://wallpapersok.com/images/hd/synthwave-grid-mountains-1qxfiiabdb5qle2q.jpg" className={styles.background} />
       </main>
     </>
   )
