2d3b24b3350d546fab0d6eb6139a487ef4bf0dda
Maxence Agullo <magullo35@gmail.com>
2025-06-06 11:06:40 +0200

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
           <img src="/images/logo.png" alt="logo of the site" className={styles.logo} />
           <h2>HyperSkate</h2>
         </nav>
+        <img src="/images/panier.svg" className={styles.shop} />
       </header>
       <main className={styles.main}>
         <p className={styles.intro}>Bienvenue au royaume des patins<br/>Depuis les années 80 le monde du transport et du sport n&apos;a cessé d&apos;évoluer. Chez HyperSkate nous avons trouvé comment subvenir à tout vos besoins.<br/>Nous vous proposons une large gamme de patins à roulettes pour tout les sports que vous pouvez pratiquer.<br/>N&apos;hésitez plus, optez pour HyperSkate.</p>
